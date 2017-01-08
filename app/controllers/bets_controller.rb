c8fa44eec6c6e787bf347ff9b8ccf508f0a329b6
michaelgt04 <michaelgt04@gmail.com>
2017-01-07 19:15:43 -0500

    Added database persistence functionality and vote counting

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -4,9 +4,33 @@ class BetsController < ApplicationController
   end
 
   def create
+    vote = params[:vote] == "Over"
+    @bet = Bet.new(
+      title: params[:bet][:title],
+      description: params[:bet][:description],
+      set_number: params[:bet][:set_number],
+      voter: params[:bet][:voter],
+      vote: vote
+    )
+    @bet.save
+    redirect_to :bets
   end
 
   def new
     @bet = Bet.new
+  end
+
+  def update
+    binding.pry
+    @bet = Bet.find(params[:id])
+    next_count = @bet.count + 1
+    @bet.update(count: next_count)
+    redirect_to :bets
+  end
+
+  def destroy
+    @bet = Bet.find(params[:id])
+    @bet.delete
+    redirect_to :bets
   end
 end
